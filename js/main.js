ddf02abec65912294c6f8b2ef903d08c48a4e0d2
nadarevd <bigboydeku@gmail.com>
2020-03-13 04:39:39 -0400

    readded commited code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+let albums = {
+  "1": "MOTS7",
+  "2": "MOTSP",
+  "3": "LYA",
+  "4": "LYT",
+  "5": "LYH",
+  "6": "YNWA",
+  "7": "Wings",
+  "8": "YoungForever",
+  "9": "HYYH2",
+  "10": "HYYH1",
+  "11": "DNW",
+  "12": "SLA",
+  "13": "ORUL8",
+  "14": "2C4S"
+
+
+}
+
+
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -22,7 +42,23 @@ function startTime() {
     } 
     return i;
   }
+
+  var img = {
+    "1": `../designs/${albums[1]}/${albums[1]} 1.png`,
+    "2": `../designs/${albums[1]}/${albums[1]} 2.png`,
+    "3": `../designs/${albums[1]}/${albums[1]} 3.png`,
+    "4": `../designs/${albums[1]}/${albums[1]} 4.png`,
+    "5": `../designs/${albums[1]}/${albums[1]} 5.png`,
+    "6": `../designs/${albums[1]}/${albums[1]} 6.png`,
+    "7": `../designs/${albums[1]}/${albums[1]} 7.png`,
+    "8": `../designs/${albums[1]}/${albums[1]} 8.png`,
+    "9": `../designs/${albums[1]}/${albums[1]} 9.png`,
+    "0": `../designs/${albums[1]}/${albums[1]} 0.png`,
+    ":": `../designs/${albums[1]}/${albums[1]} colon.png`
+  }
   
+
+
 
   function stringToImage(s) {
     //create a temporary blank variable to hold HTML images
@@ -32,18 +68,27 @@ function startTime() {
     }
     return temp
   }
-  
-  
-  var img = {
-    "1": "../numbers/MOTS 1.png",
-    "2": "../numbers/MOTS 2.png",
-    "3": "../numbers/MOTS 3.png",
-    "4": "../numbers/MOTS 4.png",
-    "5": "../numbers/MOTS 5.png",
-    "6": "../numbers/MOTS 6.png",
-    "7": "../numbers/MOTS 7.png",
-    "8": "../numbers/MOTS 8.png",
-    "9": "../numbers/MOTS 9.png",
-    "0": "../numbers/MOTS 0.png",
-    ":": "../numbers/MOTS colon.png"
+
+
+
+  function albumControll(num) {
+    img = {
+      "1": `../designs/${albums[num]}/${albums[num]} 1.png`,
+      "2": `../designs/${albums[num]}/${albums[num]} 2.png`,
+      "3": `../designs/${albums[num]}/${albums[num]} 3.png`,
+      "4": `../designs/${albums[num]}/${albums[num]} 4.png`,
+      "5": `../designs/${albums[num]}/${albums[num]} 5.png`,
+      "6": `../designs/${albums[num]}/${albums[num]} 6.png`,
+      "7": `../designs/${albums[num]}/${albums[num]} 7.png`,
+      "8": `../designs/${albums[num]}/${albums[num]} 8.png`,
+      "9": `../designs/${albums[num]}/${albums[num]} 9.png`,
+      "0": `../designs/${albums[num]}/${albums[num]} 0.png`,
+      ":": `../designs/${albums[num]}/${albums[num]} colon.png`
+    }
+    document.body.style.backgroundImage = `url('../designs/${albums[num]}/${albums[num]} Background.png')`;
   }
+
+  
+
+
+  
